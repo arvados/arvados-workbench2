96c03a921fe8e8e3c5de4333ea7d1456297f2681
Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>
2018-07-13 18:09:24 +0200

    snackbar component
    Feature #13782
    
    Arvados-DCO-1.1-Signed-off-by: Pawel Kowalczyk <pawel.kowalczyk@contractors.roche.com>

---
+++ src/components/snackbar/snackbar.tsx
@@ -0,0 +1,44 @@
+// Copyright (C) The Arvados Authors. All rights reserved.
+//
+// SPDX-License-Identifier: AGPL-3.0
+
+import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+
+type SnackbarProps = {
+    message: string
+};
+
+interface SnackbarState {
+    open: boolean;
+}
+
+export default class SnackbarComponent extends React.Component<SnackbarProps> {
+    state: SnackbarState = {
+        open: false
+    };
+
+    handleClick = () => {
+        this.setState({ open: true });
+    }
+
+    render() {
+        const { open } = this.state;
+        const { message } = this.props;
+        return (
+            <div>
+                <Button onClick={this.handleClick}>
+                    CLICK
+                </Button>
+                <Snackbar
+                    open={open}
+                    autoHideDuration={AUTO_HIDE_DURATION}
+                    message={<span id="message-id">{message}</span>}
+                />
+            </div>
+        );
+    }
+}
+
+const AUTO_HIDE_DURATION = 6000;
