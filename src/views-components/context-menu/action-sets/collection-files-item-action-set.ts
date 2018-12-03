c9a1f5adcc0722a71c3930291e2965f3a2054f7f
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-12-03 10:53:39 +0100

    Add OpenCollectionFileAction to file context menu
    Feature #13540
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/context-menu/action-sets/collection-files-item-action-set.ts
+++ src/views-components/context-menu/action-sets/collection-files-item-action-set.ts
@@ -6,9 +6,13 @@ import { ContextMenuActionSet } from "../context-menu-action-set";
 import { RenameIcon, RemoveIcon } from "~/components/icon/icon";
 import { DownloadCollectionFileAction } from "../actions/download-collection-file-action";
 import { openFileRemoveDialog, openRenameFileDialog } from '~/store/collection-panel/collection-panel-files/collection-panel-files-actions';
+import { OpenCollectionFileAction } from '~/views-components/context-menu/actions/open-collection-file-action';
 
 
 export const collectionFilesItemActionSet: ContextMenuActionSet = [[{
+    component: OpenCollectionFileAction,
+    execute: () => { return; }
+},{
     name: "Rename",
     icon: RenameIcon,
     execute: (dispatch, resource) => {
