ed73b7f27b4aaa66c1434a3cca988e33b71e3c37
Michal Klobukowski <michal.klobukowski@contractors.roche.com>
2018-12-03 10:53:08 +0100

    Set icon for DownloadCollectionFileAction
    Feature #13540
    
    Arvados-DCO-1.1-Signed-off-by: Michal Klobukowski <michal.klobukowski@contractors.roche.com>

--- src/views-components/context-menu/actions/download-collection-file-action.tsx
+++ src/views-components/context-menu/actions/download-collection-file-action.tsx
@@ -7,19 +7,22 @@ import { RootState } from "../../../store/store";
 import { DownloadAction } from "./download-action";
 import { getNodeValue } from "../../../models/tree";
 import { CollectionFileType } from "../../../models/collection-file";
+import { DownloadIcon } from '~/components/icon/icon';
 
 const mapStateToProps = (state: RootState) => {
     const { resource } = state.contextMenu;
+    const iconProp = { icon: DownloadIcon };
     if (resource) {
         const file = getNodeValue(resource.uuid)(state.collectionPanelFiles);
         if (file) {
             return {
                 href: file.url,
-                download: file.type === CollectionFileType.DIRECTORY ? undefined : file.name
+                download: file.type === CollectionFileType.DIRECTORY ? undefined : file.name,
+                ...iconProp,
             };
         }
     }
-    return {};
+    return iconProp;
 };
 
 export const DownloadCollectionFileAction = connect(mapStateToProps)(DownloadAction);
